cb37eaacb00724e5203d3bed39ae9e64fc4f6af8
taisaoko <ttd3calvin@gmail.com>
2019-05-01 17:52:18 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,10 @@
 class Dog
   attr_accessor :name, :breed, :age
   
-  def initialize
-    
+  def initialize(name, breed, age)
+    self.name = name
+    self.breed = breed
+    self.age = age
+    self.class.all << self
+  end
 end
